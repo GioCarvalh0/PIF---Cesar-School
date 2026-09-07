6a6b0bd83ede6ddc98465298b74083e6573022b0
mariagiovanna14 <mgiovannacarvalho2005@gmail.com>
2026-09-07 10:48:58 -0300

    q03

---
+++ Lista-Cap1/Q03.c
@@ -0,0 +1,14 @@
+// Questão 03:
+
+/*Documentando o processo para fazer um "Hello World" utilizando
+* comentários em várias linhas e em uma única linha também.
+***************************************************************/
+
+#include <stdio.h> // Para printf()
+
+int main() // Função main
+{ //início do corpo da função main
+    printf("Hello World"); //Chamada à função printf
+    
+    return 0; 
+} // Fim do corpo da função main
